51d375f49b2d85a4e6d35f6e6f943d49aade6682
luozhanming <459557210@qq.com>
2018-09-16 21:58:04 +0800

    修正BaseMVPFragment bug

--- mvplibrary/src/main/java/com/luozm/mvplibrary/BaseMVPFragment.java
+++ mvplibrary/src/main/java/com/luozm/mvplibrary/BaseMVPFragment.java
@@ -37,7 +37,7 @@ public abstract class BaseMVPFragment<T extends IPresenter> extends Fragment imp
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(getLayoutID(), container);
+        return inflater.inflate(getLayoutID(), null);
     }
 
     @Override
